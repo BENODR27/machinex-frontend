c9ce44c5ad224966bb6e57e206d805d3b9ceceae
BenoDavid <beno.david@classicfashion.com>
2025-11-05 13:36:46 +0300

    fix

--- src/app/features/admin/admin-routing-module.ts
+++ src/app/features/admin/admin-routing-module.ts
@@ -1,10 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './components/user-list.component/user-list.component';
+import { RoleListComponent } from './components/role-list.component/role-list.component';
+import { OrgSettingsComponent } from './components/org-settings.component/org-settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        redirectTo: 'user',
+        pathMatch: 'full',
+      },
+      {
+        path: 'user',
+        component: UserListComponent,
+        // canActivate: [RoleGuard],
+        data: { roles: ['Admin', 'Supervisor', 'Technician'] },
+      },
+      {
+        path: 'role',
+        component: RoleListComponent,
+        // canActivate: [RoleGuard],
+        data: { roles: ['Admin', 'Supervisor', 'Technician'] },
+      },
+      {
+        path: 'settings',
+        component: OrgSettingsComponent,
+        // canActivate: [RoleGuard],
+        data: { roles: ['Admin', 'Supervisor', 'Technician'] },
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
