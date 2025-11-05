e79459f1e6175f9ac10c599ad96fd25ef9c6331d
BenoDavid <beno.david@classicfashion.com>
2025-11-05 15:34:09 +0300

    sidebar work

--- src/app/layouts/admin-layout/admin-sidebar/admin-sidebar.ts
+++ src/app/layouts/admin-layout/admin-sidebar/admin-sidebar.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-sidebar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-sidebar.html',
   styleUrl: './admin-sidebar.scss',
 })
